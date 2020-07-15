b901246f94a83d74ccaff3f69e32ceb33d211cd9
danielgomezcantero <danielgomezcantero@gmail.com>
2020-07-15 16:47:39 -0400

    Configuracion de angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
 
 //Angular Material
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
